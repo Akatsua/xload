4ac52825d3cb4e0e611b37b1d761c94f717981d1
Nuno Almeida <akatsua@live.jp>
2021-02-06 21:58:00 +0000

    + Revamped diagnostic image + Fixed small bug in argument parsing

--- src/XLoad/Helpers/ImageHelper.cs
+++ src/XLoad/Helpers/ImageHelper.cs
@@ -31,36 +31,52 @@
             int[] data,
             int resolution)
         {
-            int height = data.Max();
-            int height2 = height + 1;
+            int maxTimeToPrint = data.Length > 2 * 60 * 60 ? 2 * 60 * 60 : data.Length;
 
-            Bitmap bmp = new Bitmap((uint)data.Length, (uint)height2);
+            const int padTop    = 10;
+            const int padBottom = 10;
+            const int tickSize  = 1;
+
+            int maxValue        = data.Max();
+            int maxCharNumber   = maxValue.ToString().Length;
+
+            int padLeft = maxCharNumber * 5 + 1;
+
+            Rectangle image = new Rectangle(0, 0, padLeft + data.Length, padTop + maxValue + padBottom);
+            Rectangle graph = new Rectangle(padLeft, padBottom, data.Length, maxValue);
+
+            var bitmap = new Bitmap((uint)image.Width, (uint)image.Height, true);
 
             // Background
-            for (int x = 0; x < data.Length; x++)
+            PlaceRectangle(bitmap, image, Color.White);
+
+            // Axis
+            PlaceLine(bitmap, new Point(graph.X, graph.Y), new Point(graph.X + graph.Width - 1, graph.Y), Color.Black);
+            PlaceLine(bitmap, new Point(graph.X, graph.Y), new Point(graph.X, graph.Y + graph.Height - 1), Color.Black);
+
+            PlaceNumber(bitmap, "0", graph.X - 6, graph.Y - 6);
+
+            // Horizontal Ticks
+            for (int i = resolution; i < maxTimeToPrint - 1; i+= resolution)
             {
-                for (int y = 0; y < height2; y++)
-                {
-                    bmp.SetPixel(x, y, Color.White);
-                }
+                PlaceLine(bitmap, new Point(graph.X + i, graph.Y), new Point(graph.X + i, graph.Y + tickSize), Color.Black);
             }
 
-            // Hour marks
-            for (int i = 0; i < data.Length; i += (60 * 60 / resolution))
+            // Vertical Ticks
+            int vTickSpacing = (int)Math.Floor(graph.Height / 10.0f);
+
+            for (int i = 1; i <= 10; i++)
             {
-                for (int y = 0; y < height2; y++)
-                {
-                    bmp.SetPixel(i, y, Color.OrangeRed);
-                }
+                PlaceLine(bitmap, new Point(graph.X, graph.Y + vTickSpacing * i), new Point(graph.X + graph.Width - 1, graph.Y + vTickSpacing * i), Color.FromArgb(245, 245, 255));
+                PlaceLine(bitmap, new Point(graph.X, graph.Y + vTickSpacing * i), new Point(graph.X + tickSize, graph.Y + vTickSpacing * i), Color.Black);
             }
 
-            // Message marks
-            for (int i = 0; i < height2; i += height / 20)
+            // Vertical Numbers
+
+            for (int i = 1; i <= 10; i++)
             {
-                for (int x = 0; x < data.Length; x++)
-                {
-                    bmp.SetPixel(x, height - i, Color.DarkRed);
-                }
+                var value = Math.Floor((maxValue / 10.0f) * i);
+                PlaceNumber(bitmap, value.ToString(), (maxCharNumber - value.ToString().Length) * 5, graph.Y + vTickSpacing * i - 2);
             }
 
             // Data
@@ -68,18 +84,61 @@
             {
                 if (i + 1 < data.Length)
                 {
-                    var toPaint = GetPointsOnLine(i, height - data[i], i + 1, height - data[i + 1]);
+                    var toPaint = GetPointsOnLine(i, data[i], i + 1, data[i + 1]);
 
                     foreach (var item in toPaint)
                     {
-                        bmp.SetPixel(item.X, item.Y, Color.Black);
+                        bitmap.SetPixel(item.X + graph.X, item.Y + graph.Y, Color.Black);
                     }
                 }
-
-                bmp.SetPixel(i, height - data[i], Color.Green);
             }
 
-            bmp.Save(file);
+            bitmap.Save(file);
+        }
+
+        private static void PlaceRectangle(Bitmap bitmap, Rectangle rectangle, Color color)
+        {
+            for (int x = 0; x < rectangle.Width; x++)
+            {
+                for (int y = 0; y < rectangle.Height; y++)
+                {
+                    bitmap.SetPixel(x + rectangle.X, y + rectangle.Y, Color.White);
+                }
+            }
+        }
+
+        private static void PlaceLine(Bitmap bitmap, Point p1, Point p2, Color color)
+        {
+            foreach (var point in GetPointsOnLine(p1.X, p1.Y, p2.X, p2.Y))
+            {
+                bitmap.SetPixel(point.X, point.Y, color);
+            }
+        }
+
+        private static void PlaceNumber(Bitmap bitmap, string number, int cornerX, int cornerY)
+        {
+            for (int i = 0; i < number.Length; i++)
+            {
+                PlaceDigit(bitmap, number[i], cornerX + (i * 5), cornerY);
+            }
+        }
+
+        private static void PlaceDigit(Bitmap bitmap, char letter, int cornerX, int cornerY)
+        {
+            byte[] rectangle = new byte[20];
+
+            Font.Get4x5PointArray(letter, ref rectangle, true);
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    if ((rectangle[(4 * y) + x] & 1) == 1)
+                    {
+                        bitmap.SetPixel(cornerX + x, cornerY + y, Color.Blue);
+                    }
+                }
+            }
         }
 
         // From Eric Woroshow
